8ecff555838b94fad03906c1a6056156246a4055
Rendroid <renierjohnsediego@gmail.com>
2020-05-10 10:34:42 +0800

    clean code

--- modules/custom/random_sort/src/Controller/JsonController.php
+++ modules/custom/random_sort/src/Controller/JsonController.php
@@ -8,22 +8,14 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Drupal\user\Entity\User;
 use Drupal\user\UserInterface;
 use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Language\LanguageManagerInterface;
-use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
-use Drupal\Core\StringTranslation\TranslationInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Entity\EntityFieldManagerInterface;
 use Drupal\Core\Utility\Token;
-use Drupal\Core\Transliteration\PhpTransliteration;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\EventDispatcher\GenericEvent;
 use Drupal\Core\Entity\EntityStorageException;
 use Drupal\Component\Utility\Unicode;
 use  Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\Serializer\Serializer;
-use Drupal\Core\Access\CsrfTokenGenerator;
 /**
  * Class MyWebService.
  */
@@ -33,38 +25,19 @@ class JsonController extends ControllerBase {
   const ACCESS_TOKEN = "EAADJLbNQ3vwBAIJhy7ZCPHZA2nCJFB7acsTv9JK5jeFILp9ZA5aJmPfTflIGTiQVs7WUJuzkRKhqhVDz3N5ubZCITYUCrTPkbrpSdgGQI7XKQPQxJpflVRa9wp53eXOcmqzD528hAXGjUiZCU7QYFID07gHNI2cRz0K8ZA79DoJs8ROZBZCoeZCX5LlBZBlNimrmoZD";
 
 
-
-    // $reply_message  = [
-    //     "messaging_type"=>"UPDATE",
-    //     "recipient"=>{
-    //       "id": "3769505946454041"
-    //     },
-    //     "message": {
-    //       "text": "hello, renier ,Thanks for visit mywebsite!"
-    //     }
-    //   ];
-
   public function webhook(){
       $request = \Drupal::request();
       $challenge = $request->get('hub_challenge');
       $fb_token = $request->get('hub_verify_token');
-
       // $data['query'] = $request->query->all();
       // $data['header'] = $request->headers->all();
       // $data['request'] = $request->request->all();
-
-
       $data  = file_get_contents("php://input");
-      // $text = json_decode($data,true);
-      // $text[]
-      // $data = $request->getContent();
-      // file_put_contents("sites/default/files/data.txt",$data);
       file_put_contents("sites/default/files/data/".Date("H-i-s").".txt",$data);
 
       if($fb_token == JsonController::VERIFY_TOKEN){
         $response = new Response($challenge);
         $response->setStatusCode(200);
-        // $response->sendBody($challenge);
         return $response;
       }
 
@@ -72,9 +45,7 @@ class JsonController extends ControllerBase {
       $response->setStatusCode(200);
       return $response;
   }
-
   public function send_message(){
-
         $request = \Drupal::request();
         $message = $request->get('message');
         $text_message = [
@@ -86,11 +57,6 @@ class JsonController extends ControllerBase {
         $client = \Drupal::httpClient();
         $request = $client->request('POST',$url,$text_message);
         $status = $request->getStatusCode();
-        // $data = file_get_contents("php://input");
-        // $data['query'] = $request->query->all();
-        // $data['header'] = $request->headers->all();
-        // $data['request'] = $request->request->all();
-
         file_put_contents("sites/default/files/data/".Date("H-i-s").".txt",$status);
         return $status;
   }
@@ -122,16 +88,13 @@ class JsonController extends ControllerBase {
     curl_setopt($ch, CURLINFO_HEADER_OUT, true);
     curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-
-      // Set HTTP Header for POST request
-      curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
         'Content-Type: application/json',
         'Content-Length: ' . strlen($payload))
       );
-
-      $result = curl_exec($ch);
-      curl_close($ch);
-      return $result;
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
   }
 
 
@@ -147,8 +110,6 @@ class JsonController extends ControllerBase {
    *   A response which contains the ID and CSRF token.
    */
   public function login_fb() {
-    // return new Response('',200,new Cookie('SESSc8a29b3530221fefd97ca0f2616a2096', 'ZUVMqiSmHj2LKqNqBxXlIMiVRMxFZLazPZ9lkH_ckKE'));
-
     $request = \Drupal::request();
     $output['id'] = $request->get('id');
     $output['uname'] = urldecode($request->get('uname'));
@@ -169,42 +130,13 @@ class JsonController extends ControllerBase {
       $response->setStatusCode(309);
       $response->sendHeaders();
     }
-    //     // $session = new \Symfony\Component\HttpFoundation\Session\Session();
-    //     // $session->set('uid',94);
-    //     // $session->start();
-    //     // SESSc8a29b3530221fefd97ca0f2616a2096 ZUVMqiSmHj2LKqNqBxXlIMiVRMxFZLazPZ9lkH_ckKE
-    //     return $response;
-    //     // $response->headers->setCookie(new Cookie('SESSc8a29b3530221fefd97ca0f2616a2096', 'ZUVMqiSmHj2LKqNqBxXlIMiVRMxFZLazPZ9lkH_ckKE'));
-    //     // $response->send();
-    //     // return new JsonResponse('ok');
-
     else{
-      $user = $this->createUser($output['id'],$output['uname'],$output['email'],$output['profile_picture']);
-      // $this->userLoginFinalize($user);
-
-      // $autoloader = require_once 'autoload.php';
-      // $kernel = new DrupalKernel('prod', $autoloader);
-
-
-      // $query->condition('field_fb_id', $output['id']);
-      // $uid = $query->execute();
+      $this->createUser($output['id'],$output['uname'],$output['email'],$output['profile_picture']);
       $uid = \Drupal::service('user.auth')->authenticate($output['uname'], '1234');
       $user = \Drupal\user\Entity\User::load($uid);
       user_login_finalize($user);
       $response->send();
       return $response;
-      // $format = $this->getRequestFormat($request);
-      // $content = $request->getContent();
-      //
-      // $response_data = [];
-      // $response_data['current_user']['uid'] = $id;
-      // $response_data['current_user']['roles'] = 'guest';
-      // $response_data['current_user']['name'] = $output['uname'];
-      // $response_data['csrf_token'] = $this->csrfToken->get('rest');
-      // $encoded_response_data = $this->serializer->encode($response_data, 'json');
-      // return new Response($encoded_response_data);
-
-      // return $response;
       }
 
   }
@@ -224,7 +156,6 @@ private function createUser($fb_id = "" ,$username = "",$email = "",$profile_pic
   $user->addRole('guest');
   $user->activate();
   $res = $user->save();
-  // $this->loggerFactory->warning($res);
   return $res;
  }
 }
