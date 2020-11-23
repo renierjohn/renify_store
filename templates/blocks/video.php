a25e4a44a0a609bf427b95726ee7e581f06bacc3
Renier <renierjohnsedie@gmail.com>
2020-11-23 17:30:15 +0800

    update content

--- templates/blocks/video.php
+++ templates/blocks/video.php
@@ -27,11 +27,11 @@
     </div>
 
     <div class="row process-wrap">
-        <h2 class="display-2 text-center" data-aos="fade-up">How The Order Works?</h2>
+        <h2 class="display-2 text-center" data-aos="fade-up">How to Order ?</h2>
         <div class="process" data-aos="fade-up">
             <div class="process__steps block-1-2 block-tab-full group">
                 <div class="col-block step">
-                    <h3 class="item-title">Add-to-Cart</h3>
+                    <h3 class="item-title">AddtoCart</h3>
                     <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
                     </p>
                 </div>
--- templates/main.php
+++ templates/main.php
@@ -3,3 +3,19 @@
 <?php $this->insert('./blocks/video')?>
 <?php $this->insert('./blocks/feature')?>
 <?php $this->insert('./blocks/pricing')?>
+
+<div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+  <div class="toast-header">
+    <svg class=" rounded mr-2" width="20" height="20" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice"
+      focusable="false" role="img">
+      <rect fill="#007aff" width="100%" height="100%" /></svg>
+    <strong class="mr-auto">Bootstrap</strong>
+    <small class="text-muted">11 mins ago</small>
+    <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+  <div class="toast-body">
+    Hello, world! This is a toast message.
+  </div>
+</div>
